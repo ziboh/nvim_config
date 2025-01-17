24ea942d164157de7854b79145e6d65d835a6bee
ziboh <sharezhou@outlook.com>
2025-01-18 01:45:09 +0800

    markdown enabled

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,7 +2,7 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    enabeld = Utils.is_remote(),
+    enabled = not Utils.is_remote(),
     build = function()
       require("lazy").load({ plugins = { "markdown-preview.nvim" } })
       vim.fn["mkdp#util#install"]()
