9f6a282fa720dbb123d5ff6f0cb641a70469cd80
ziboh <sharezhou@outlook.com>
2025-01-21 00:57:49 +0800

    powershell

--- lua/options.lua
+++ lua/options.lua
@@ -127,4 +127,14 @@ if vim.fn.has("win32") == 1 then
   else
     return Utils.error("No powershell executable found")
   end
+  vim.o.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+
+  vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+
+  vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+
+  vim.o.shellquote = ""
+
+  vim.o.shellxquote = ""
 end
